6067e393d1cf498ebe7964f76d15f80c9d948d2e
excellenceakeke06-ui <excellenceakeke06@gmail.com>
2025-12-26 17:13:54 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,16 @@
+
+// variables
+
+// variable defination
+
+// var , let , const
+
+var myname='EXCELLENCE';
+
+let favouritecolour = 'black'
+
+const PI = 3.142
+
+console.log(myname);
+console.log(favouritecolour)
+console.log(PI)
