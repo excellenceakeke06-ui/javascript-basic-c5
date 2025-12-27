fc12f09f2f3896a6a447c8f92101b0721f4de6c1
excellenceakeke06-ui <excellenceakeke06@gmail.com>
2025-12-27 17:30:27 +0100

    Add files via upload
    updated

--- intro.js
+++ intro.js
@@ -8,9 +8,71 @@
 var myname='EXCELLENCE';
 
 let favouritecolour = 'black'
-
+let ismarried = false
+let lovesfood = true
 const PI = 3.142
+let favouriteclub;
+myname = "gambo"
+ismarried =true
 
+console.log(favouritecolour);
 console.log(myname);
-console.log(favouritecolour)
-console.log(PI)
+console.log(ismarried);
+// data types
+
+// 1 fundamental data types
+// 2 Reference data types
+
+//Fundamental data types
+
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. BigInt
+// 7. Symbol
+
+//STRING ; any word inside a quotation mark is a string , "a" , "baba" ETC
+
+//NUMBER ; any numeric value is a number 1,2,3,4,-1.5,-3,1.5 ETC
+
+//BOOLEAN ; it can have only two values true or false
+
+//operation on datatypes
+
+// 1. Arithmetic operation
+
+let x = 10
+
+let c = 4
+
+let y = 20
+
+let z = x + y
+let a = y - x
+let b = x * y
+let d = Math.sqrt(c)
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(d);
+
+//operation on string
+
+//concatination
+
+let firstname = "Excellence"
+let lastname = "Akeke"
+
+let fullname = lastname +" "+ firstname
+
+console.log(fullname);
+
+intro = "My name is "  +" "  + fullname
+
+console.log(intro)
+
+let fullnamelower = fullname.toLowerCase()
+
+console.log(fullnamelower);
