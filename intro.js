0269c4270c41724a5ec46988fda425c8eb1c2a54
excellenceakeke06-ui <excellenceakeke06@gmail.com>
2025-12-31 02:33:21 +0100

    Add files via upload

--- intro.js
+++ intro.js
@@ -15,9 +15,9 @@ let favouriteclub;
 myname = "gambo"
 ismarried =true
 
-console.log(favouritecolour);
-console.log(myname);
-console.log(ismarried);
+//console.log(favouritecolour);
+//console.log(myname);
+//console.log(ismarried);
 // data types
 
 // 1 fundamental data types
@@ -49,14 +49,14 @@ let c = 4
 
 let y = 20
 
-let z = x + y
-let a = y - x
-let b = x * y
-let d = Math.sqrt(c)
-console.log(z);
-console.log(a);
-console.log(b);
-console.log(d);
+//let z = x + y
+//let a = y - x
+//let b = x * y
+//let d = Math.sqrt(c)
+//console.log(z);
+//console.log(a);
+//console.log(b);
+//console.log(d);
 
 //operation on string
 
@@ -67,12 +67,124 @@ let lastname = "Akeke"
 
 let fullname = lastname +" "+ firstname
 
-console.log(fullname);
+//console.log(fullname);
 
 intro = "My name is "  +" "  + fullname
 
-console.log(intro)
+//console.log(intro)
 
 let fullnamelower = fullname.toLowerCase()
 
-console.log(fullnamelower);
+//console.log(fullnamelower);
+
+samplestring = "i love food".toUpperCase()
+
+console.log(samplestring);
+
+let stringlength = samplestring.length
+
+//console.log(stringlength);
+
+let indexofstring = samplestring.indexOf("FOOD")
+//console.log(indexofstring)
+
+//operation on boolean
+
+let age = 16
+let legalage = 18
+gender  = "male"
+
+// AND , OR , NOT operations
+
+console.log(age>legalage);
+
+// AND && operation both conditions must be true
+
+//console.log(true && true);
+//console.log(true && false);
+//console.log(false && false);
+
+//console.log(age>=legalage && "i can drink alcohol")
+
+// OR || operation one condition must be true
+
+//console.log(true || false);
+//console.log(false || false);
+//console.log(true || true);
+
+
+
+console.log(age>=legalage || gender == "male"&& "i can drink alcohol");
+
+// NOT ! operation reverses the condition
+
+console.log(!ismarried);
+
+// TRUTHY AND FALSY VALUES
+
+//truthy values : strings , numbbers , objects , arrays , true boolean
+//falsy values : undefined , null , false boolean , 0 number , NaN
+
+//CONDITIONALS
+
+//if(){
+
+//}
+//else {
+
+//
+
+if(true){
+    console.log("i am happy".toUpperCase());
+}
+else {
+     console.log("i am sad".toUpperCase());
+}
+
+
+let pass = 50
+let score = 75
+
+if(score >= pass){
+    console.log("you have passed the exam".toUpperCase());
+}
+else {
+     console.log("you have failed the exam".toUpperCase());
+}
+
+let temperature = 22
+if(temperature<20){
+    console.log("it is cold outside".toUpperCase());
+}
+else{
+    console.log("it is warm outside".toUpperCase());
+}
+
+// let a =12
+// let b = 15 
+// console.log(a>b ? "a is greater than b" : "b is greater than a");
+
+// TERNARY OPERATOR
+// condition ? value if true : value if false
+// condition ?
+// value if true :
+// value if false
+// EXAMPLE
+let number = 1
+ console.log(number % 2 ==0 ? "number is even" : "number is odd");
+// EXAMPLE
+//  let marks = 65
+//  console.log(marks >=50 ? "you have failed the exam" : "you have passed the exam");
+// EXAMPLE
+//   time = 18
+//  console.log(time < 12 ? "good morning" : "good afternoon");
+// EXAMPLE
+// let loggedin = true
+// console.log(loggedin ? "welcome back user" : "please log in");
+// let number = 38
+// console.log(number % 2 == 0 ? "number is even" : "number is odd");
+
+// let a = 2
+// let b = 15
+
+// console.log(a*b ?  "it is 17 ": "it is not 17");
